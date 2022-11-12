1b6431104c6842208c4e64c24c397d637345266f
Michal Žamboch <zamboch.michal@seznam.cz>
2022-11-12 21:42:11 +0100

    :)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
 extern crate keepass;
+extern crate kdbx_rs;
 
+use keepass::{Database, NodeRef, Result, Error};
+use std::fs::File;
 
 fn main() {
-    println!("Hello, world!");
+
+    test1();
+}
+
+fn test1() -> Result<()> {
+    
+    println!("Hello world");
+
+    // Open KeePass database
+    let path = std::path::Path::new("tests/resources/test_db_with_password.kdbx");
+    let db = Database::open(&mut File::open(path)?, Some("demopass"), None)?;
+
+    // Iterate over all Groups and Nodes
+    for node in &db.root {
+        match node {
+            NodeRef::Group(g) => {
+                println!("Saw group '{0}'", g.name);
+            },
+            NodeRef::Entry(e) => {
+                let title = e.get_title().unwrap();
+                let user = e.get_username().unwrap();
+                let pass = e.get_password().unwrap();
+                println!("Entry '{0}': '{1}' : '{2}'", title, user, pass);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn test2() {
+    
 }
