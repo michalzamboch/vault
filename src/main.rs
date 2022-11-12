d087beb36470ad8ef98c93a4c8d13a5fead94871
Michal Žamboch <zamboch.michal@seznam.cz>
2022-11-12 22:43:39 +0100

    new tests

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,97 @@
-extern crate keepass;
-extern crate kdbx_rs;
+use kdbx_rs::{self, CompositeKey, Error, Database};
+use kdbx_rs::Kdbx;
 
-use keepass::{Database, NodeRef, Result, Error};
+use kdbx_rs::binary::InnerStreamCipherAlgorithm;
+use kdbx_rs::database::{Entry, Times};
+use kdbx_rs::xml::write_xml;
+
+use chrono::NaiveDate;
+use std::fs::read_to_string;
+use std::path::PathBuf;
+use uuid::Uuid;
+
 use std::fs::File;
 
 fn main() {
 
-    test1();
 }
 
-fn test1() -> Result<()> {
-    
-    println!("Hello world");
+fn test1() -> Result<(), Error> {
+    let file_path = "./res/test_input/kdbx4-argon2.kdbx";
+    let kdbx = kdbx_rs::open(file_path)?;
+    let key = CompositeKey::from_password("kdbxrs");
+    let unlocked = kdbx.unlock(&key)?;
 
-    // Open KeePass database
-    let path = std::path::Path::new("tests/resources/test_db_with_password.kdbx");
-    let db = Database::open(&mut File::open(path)?, Some("demopass"), None)?;
+    let password = unlocked.find_entry(|e| e.url() == Some("https://example.com"))
+        .unwrap()
+        .password();
 
-    // Iterate over all Groups and Nodes
-    for node in &db.root {
-        match node {
-            NodeRef::Group(g) => {
-                println!("Saw group '{0}'", g.name);
-            },
-            NodeRef::Entry(e) => {
-                let title = e.get_title().unwrap();
-                let user = e.get_username().unwrap();
-                let pass = e.get_password().unwrap();
-                println!("Entry '{0}': '{1}' : '{2}'", title, user, pass);
-            }
-        }
-    }
-
+        println!("{}", password.unwrap());
     Ok(())
 }
 
-fn test2() {
-    
+fn new_db() {
+    let mut database = Database::default();
+    database.set_name("My First Database");
+    database.set_description("Created with kdbx-rs");
+
+    let mut entry = Entry::default();
+    entry.set_password("password1");
+    entry.set_url("https://example.com");
+    entry.set_username("User123");
+
+    database.add_entry(entry);
+
+    let mut kdbx = Kdbx::from_database(database);
+    kdbx.set_key(CompositeKey::from_password("foo123"));
+
+    let mut file = File::create("/tmp/kdbx-rs-example.kdbx");
+    //kdbx.write(&mut file);
+}
+
+
+fn set_sample_times(times: &mut Times) {
+    times.last_access_time = NaiveDate::from_ymd_opt(2020, 5, 1).expect("REASON").and_hms(1, 2, 3);
+    times.last_modification_time = NaiveDate::from_ymd(2020, 4, 1).and_hms(1, 2, 3);
+    times.creation_time = NaiveDate::from_ymd(2020, 4, 1).and_hms(1, 1, 3);
+    times.location_changed = NaiveDate::from_ymd(2020, 4, 1).and_hms(1, 1, 3);
+    times.expiry_time = NaiveDate::from_ymd(2020, 4, 1).and_hms(1, 1, 3);
+    times.expires = false;
+    times.usage_count = 1;
+}
+
+#[test]
+fn generate_xml() -> Result<(), kdbx_rs::Error> {
+    let mut expected_path = PathBuf::new();
+    expected_path.push(env!("CARGO_MANIFEST_DIR"));
+    expected_path.push("res");
+    expected_path.push("test_output");
+    expected_path.push("generate_xml.xml");
+
+    let expected_xml_string = read_to_string(expected_path).unwrap().replace("\r\n", "\n");
+
+    let mut db = kdbx_rs::Database::default();
+    db.set_name("BarName");
+    db.set_description("BazDesc");
+    let group = db.root_mut();
+    group.set_name("FooGroup");
+    group.set_uuid(Uuid::from_u128(0x12345678));
+    set_sample_times(group.times_mut());
+    let mut entry = Entry::default();
+    entry.set_title("Bar");
+    entry.set_password("kdbxrs");
+    entry.set_uuid(Uuid::from_u128(0x654321));
+    set_sample_times(entry.times_mut());
+    group.add_entry(entry);
+
+    let mut output_buffer = Vec::new();
+
+    let key = vec![0xA0; 16];
+    let mut stream_cipher = InnerStreamCipherAlgorithm::ChaCha20
+        .stream_cipher(&key)
+        .unwrap();
+    write_xml(&mut output_buffer, &db, stream_cipher.as_mut())?;
+    let xml_string = String::from_utf8(output_buffer).unwrap();
+    assert_eq!(expected_xml_string, xml_string);
+    Ok(())
 }
