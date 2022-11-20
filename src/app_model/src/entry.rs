5cc70e4a3ddce7ba3285b69d8f98c71e34ebe5c0
Michal Žamboch <zamboch.michal@seznam.cz>
2022-11-20 22:13:26 +0100

    Improvements. Entry wraper.

--- src/app_model/src/entry.rs
+++ src/app_model/src/entry.rs
@@ -1,22 +1,64 @@
-#![allow(dead_code, unused_variables, non_camel_case_types)]
+#![allow(dead_code, unused_variables, non_camel_case_types, unused_imports)]
 
-pub struct Entry {
-    id: u32,
+use kdbx_rs::{self, CompositeKey, Error, Database, Kdbx};
+use kdbx_rs::binary::InnerStreamCipherAlgorithm;
+use kdbx_rs::database::{Entry, Times};
+use kdbx_rs::xml::write_xml;
+
+use chrono::NaiveDate;
+use std::fs::{read_to_string, File};
+use std::path::PathBuf;
+use uuid::Uuid;
+
+pub struct EntryWrapper {
+    id: i32,
+    entry: Entry,
+    time: Times,
     title: String,
     password: String,
     url: Option<String>,
     note: Option<String>,
 }
 
-impl Entry {
-    pub fn new(title: String, password: String) -> Self {
+impl EntryWrapper {
+    pub fn new(title: &str, password: &str) -> Self {
         Self { 
-            id: 0,
-            title: title,
+            id: -1,
+            entry: Entry::default(),
+            time: Times::default(),
+            title: title.to_owned(),
             password: password.to_owned(),
             url: None,
             note: None,
         }
+    }
+
+    pub fn get_id(self) -> i32 {
+        self.id
+    }
+
+    pub fn set_title(&mut self, title: String) {
+        self.title = title;
+    }
+
+    pub fn get_title(self) -> String {
+        self.title
+    }
+
+    pub fn set_password(&mut self, password: String) {
+        self.password = password;
+    }
+    
+    pub fn get_password(self) -> String {
+        self.title
+    }
+
+    pub fn set_url(&mut self, url: String) {
+        self.url = Some(url);
+    }
+    
+    pub fn set_note(&mut self, note: String) {
+        self.note = Some(note);
     }
   
 }
