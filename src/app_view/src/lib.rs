95fbf43a689df22e80c39d3c39eca5dc99292039
Michal Žamboch <zamboch.michal@seznam.cz>
2022-12-04 17:59:02 +0100

    added libraries

--- src/app_view/src/lib.rs
+++ src/app_view/src/lib.rs
@@ -2,7 +2,29 @@
 
 use app_controller::*;
 
+use iced::alignment::{self, Alignment};
+use iced::event::{self, Event};
+use iced::keyboard;
+use iced::subscription;
+use iced::theme::{self, Theme};
+use iced::widget::{
+    self, button, checkbox, column, container, row, scrollable, text,
+    text_input, Text,
+};
+use iced::window;
+use iced::{Application, Element};
+use iced::{Color, Command, Font, Length, Settings, Subscription};
+
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+
+static INPUT_ID: Lazy<text_input::Id> = Lazy::new(text_input::Id::unique);
+
+// -----------------------------------------------------------------------
+
 pub fn start() {
     println!("App start...");
     test_controller_start();
 }
+
+// -----------------------------------------------------------------------
