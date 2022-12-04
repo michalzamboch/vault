7226e4ffa066db75ec94fda0632ebf7c1f7dad5b
Michal Žamboch <zamboch.michal@seznam.cz>
2022-12-04 22:06:48 +0100

    first gui steps

--- src/app_view/src/lib.rs
+++ src/app_view/src/lib.rs
@@ -2,7 +2,7 @@
 
 use app_controller::*;
 
-use iced::alignment::{self, Alignment};
+use iced::alignment::{self};
 use iced::event::{self, Event};
 use iced::keyboard;
 use iced::subscription;
@@ -11,20 +11,78 @@ use iced::widget::{
     self, button, checkbox, column, container, row, scrollable, text,
     text_input, Text,
 };
-use iced::window;
-use iced::{Application, Element};
-use iced::{Color, Command, Font, Length, Settings, Subscription};
+use iced::{Alignment, Element, Sandbox, Settings};
 
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
-static INPUT_ID: Lazy<text_input::Id> = Lazy::new(text_input::Id::unique);
-
 // -----------------------------------------------------------------------
+
+fn run_view() -> iced::Result {
+    Counter::run(Settings::default())
+}
 
 pub fn start() {
     println!("App start...");
     test_controller_start();
+    
+    let result = run_view();
 }
 
 // -----------------------------------------------------------------------
+
+struct Counter {
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self { value: 0 }
+    }
+
+    fn title(&self) -> String {
+        String::from("Vault")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 10;
+            }
+            Message::DecrementPressed => {
+                self.value -= 10;
+            }
+        }
+    }
+    
+    fn view(&self) -> Element<Message> {
+        column![
+            button("Increment").on_press(Message::IncrementPressed),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::DecrementPressed),
+            search_bar()
+        ]
+        .padding(20)
+        .align_items(Alignment::Center)
+        .into()
+    }
+}
+
+
+fn search_bar() -> Element<'static, Message> {
+    row![
+        text("Text").size(50),
+        button("Search").on_press(Message::DecrementPressed)
+    ]
+    .padding(20)
+    .align_items(Alignment::Center)
+    .into()
+}
