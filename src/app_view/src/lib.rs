7fb12bfef0a5b80cc239100fed0f68af3fec215e
Michal Žamboch <zamboch.michal@seznam.cz>
2022-12-11 22:13:50 +0100

    using new ui framework - egui

--- src/app_view/src/lib.rs
+++ src/app_view/src/lib.rs
@@ -2,108 +2,73 @@
 
 use app_controller::*;
 
-use iced::alignment::{self};
-use iced::event::{self, Event};
-use iced::keyboard;
-use iced::subscription;
-use iced::theme::{self, Theme};
-use iced::widget::{
-    self, button, checkbox, column, container, row, scrollable, text,
-    text_input, Text,
-};
-use iced::{Alignment, Element, Sandbox, Settings};
-
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
-
-use eframe::egui::{*};
+use eframe::egui;
 
 // -----------------------------------------------------------------------
-
-fn run_view() -> iced::Result {
-    Counter::run(Settings::default())
-}
 
 pub fn start() {
     println!("App start...");
     test_controller_start();
-    
-    let result = run_view();
+
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(800.0, 600.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Confirm exit",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    );
 }
 
-// -----------------------------------------------------------------------
-
-struct Counter {
-    value: i32,
-    searchedString: String,
+#[derive(Default)]
+struct MyApp {
+    allowed_to_close: bool,
+    show_confirmation_dialog: bool,
+    search_string: String,
 }
 
-#[derive(Debug, Clone)]
-enum Message {
-    IncrementPressed,
-    DecrementPressed,
-    Search(String)
-}
+impl eframe::App for MyApp {
+    fn on_close_event(&mut self) -> bool {
+        self.show_confirmation_dialog = true;
+        self.allowed_to_close
+    }
 
-impl Sandbox for Counter {
-    type Message = Message;
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Vault");
 
-    fn new() -> Self {
-        Self {
-            value: 0,
-            searchedString: String::new(),
+            let text_edit_search = egui::TextEdit::singleline(&mut self.search_string)
+                .desired_width(f32::INFINITY)
+                .cursor_at_end(true);
+            let response = ui.add( text_edit_search);
+            if response.changed() {
+                println!("Changed: \"{}\"", self.search_string);
+            }
+            if response.lost_focus() && ui.input().key_pressed(egui::Key::Enter) {
+                println!("Entered: \"{}\"", self.search_string);
+            }
+        });
+
+        if self.show_confirmation_dialog {
+            egui::Window::new("Do you want to quit?")
+                .collapsible(false)
+                .resizable(false)
+                .show(ctx, |ui| {
+                    ui.horizontal(|ui| {
+                        if ui.button("Cancel").clicked() {
+                            self.show_confirmation_dialog = false;
+                        }
+
+                        if ui.button("Yes!").clicked() {
+                            self.allowed_to_close = true;
+                            frame.close();
+                        }
+                    });
+                });
         }
     }
-
-    fn title(&self) -> String {
-        String::from("Vault")
-    }
-
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::IncrementPressed => {
-                self.value += 10;
-            }
-            Message::DecrementPressed => {
-                self.value -= 10;
-            }
-            Message::Search(value) => {
-                self.searchedString.clear();
-                self.searchedString = value;
-                println!("{}", self.searchedString)
-            }
-        }
-    }
-    
-    fn view(&self) -> Element<Message> {
-        column![
-            button("Increment").on_press(Message::IncrementPressed),
-            text(self.value).size(50),
-            button("Decrement").on_press(Message::DecrementPressed),
-            search_bar()
-        ]
-        .padding(20)
-        .align_items(Alignment::Center)
-        .into()
-    }
-}
-
-
-fn search_bar() -> Element<'static, Message> {
-    
-    let mut value: String = String::new();
-    row![
-        text("Text").size(50),
-        button("Search").on_press(Message::DecrementPressed),
-        text_input(
-            "Type something to continue...",
-            &value,
-            Message::Search)
-        .padding(10)
-        .size(10)
-    ]
-    .padding(20)
-    .align_items(Alignment::Center)
-    .into()
 }
